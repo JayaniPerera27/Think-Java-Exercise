553821a3df22a35c21553f2b6b43b0c91842b122
JayaniPerera27 <pererajayani27@gmail.com>
2025-05-14 07:48:24 +0530

    Exercises_10

--- Exercises_10/Question2.java
+++ Exercises_10/Question2.java
@@ -12,6 +12,7 @@ public class Question2 {
     }
     public static Point findCenter(Rectangle box) {
         int x = box.x + box.width / 2;
+
         int y = box.y + box.height / 2;
         return new Point(x, y);
     }
